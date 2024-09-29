040420fc6599223ec0b6a6550506795378414bb6
CRUFTY <crufty01@gmail.com>
2024-09-29 12:58:40 -0300

    Add project files.

---
+++ Fabrica_computadoras/Computadora.vb
@@ -0,0 +1,78 @@
+﻿Public Class Computadora
+    Public Const Intel As Short = 10000
+    Public Const AMD As Short = 9000
+    Public Const GigaRAM As Short = 50
+    Public Const GigaDisco As Short = 5
+    Public Const Pulgada As Short = 30
+    Public Const HoraBat As Short = 5
+    Public Const GigaVideo As Short = 10
+
+    Private Modelo As String
+    Private Cpu As Char
+    Private Memoria_ram As Integer
+    Private Disco As Integer
+
+    Public Property GetModelo() As String
+        Get
+            Return Modelo
+        End Get
+        Set(value As String)
+            If value.Length <= 10 Then
+                Modelo = value
+            Else
+                Throw New ArgumentException("El modelo no puede tener más de 10 caracteres")
+            End If
+        End Set
+    End Property
+
+    Public Property GetCpu() As String
+        Get
+            Return Cpu
+        End Get
+        Set(value As String)
+            If value = "I" OrElse value = "A" Then
+                Cpu = Char.ToUpper(value)
+            Else
+                Throw New ArgumentException("CPU debe ser 'I' para Intel o 'A' para AMD.")
+            End If
+        End Set
+    End Property
+    Public Property GetDisco() As Integer
+        Get
+            Return Disco
+        End Get
+        Set(value As Integer)
+            Disco = value
+        End Set
+    End Property
+
+    Public Sub IngresarDatos()
+        Console.Write("Ingrese el modelo (max 10 caracteres): ")
+        Modelo = Console.ReadLine()
+
+        Console.Write("Ingrese CPU (I para Intel, A para AMD): ")
+        Cpu = Convert.ToChar(Console.ReadLine())
+
+        Console.Write("Ingrese cantidad de Memoria RAM (8 a 64 GB): ")
+        Memoria_ram = Convert.ToInt32(Console.ReadLine())
+
+        Console.Write("Ingrese capacidad de Disco (en GB): ")
+        Disco = Convert.ToInt32(Console.ReadLine())
+    End Sub
+
+    Public Function Precio() As Integer
+        Dim precioBase As Integer
+        If Cpu = "I" Then
+            precioBase = Intel
+        Else
+            precioBase = AMD
+        End If
+
+        precioBase += Memoria_ram * GigaRAM
+        precioBase += Disco * GigaDisco
+
+
+        Return precioBase
+    End Function
+
+End Class
---
+++ Fabrica_computadoras/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim opcion As Char
+        Do
+            Console.WriteLine("Menú de opciones:")
+            Console.WriteLine("
+            1- Fabricar Computadora
+            2- Fabricar  Notebook
+            3- Fabricar Notebook Gamer
+            0- Salir
+            ")
+            opcion = Char.Parse(Console.ReadLine())
+
+            Select Case opcion
+                Case "1"
+                    Dim computadora As New Computadora()
+                    computadora.IngresarDatos()
+                    Console.WriteLine("El precio de la computadora es: $" & computadora.Precio())
+                Case "2"
+                    'Dim notebook As New Notebook()
+                    'notebook.IngresarDatos()
+                    'Console.WriteLine("El precio de la notebook es: $" & notebook.Precio())
+                Case "3"
+                    'Dim gamer As New NoteGamer()
+                    'gamer.IngresarDatos()
+                    'Console.WriteLine("El precio de la notebook gamer es: $" & gamer.Precio())
+                Case Else
+                    Console.WriteLine("Opcion inválida")
+            End Select
+        Loop While opcion <> "0"
+
+    End Sub
+
+End Module
